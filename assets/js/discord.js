9e3d2b8e3ee6ea1ef8b52fce0d49d5a8f564be39
Abhinav2011VS <abhinav2011vs@gmail.com>
2024-12-22 21:51:55 +0530

    Update Discord

--- assets/js/discord.js
+++ assets/js/discord.js
@@ -1,7 +1,8 @@
 (function () {
     const currentPath = window.location.pathname;
 
-    if (currentPath === "/discord/") {
+    // Check if the path is /discord or /discord/
+    if (currentPath === "/discord" || currentPath === "/discord/") {
         // Redirect to the Discord link
         window.location.href = "https://discord.gg/F2qMKBu5ea";
     }
