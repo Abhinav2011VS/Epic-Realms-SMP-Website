9ffa710b9451a012f6df5b41d8813b5bcac6fc0b
Abhinav2011VS <abhinav2011vs@gmail.com>
2024-12-22 21:49:37 +0530

    Add discord feature

---
+++ assets/js/discord.js
@@ -0,0 +1,9 @@
+(function () {
+    const currentPath = window.location.pathname;
+
+    if (currentPath === "/discord/") {
+        // Redirect to the Discord link
+        window.location.href = "https://discord.gg/F2qMKBu5ea";
+    }
+    // Else, do nothing
+})();
